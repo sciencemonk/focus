e8c2bdbe6ec3cd05da3c4aea0d92549a45842e0a
Michael Olaya <35185465+sciencemonk@users.noreply.github.com>
2018-01-12 12:06:12 -0600

    Add files via upload

---
+++ audioAssets.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Audio Source - Qigo soundcloud
+var audioData = [
+    {
+        'title' : 'Level 1',
+        'url' : 'https://feeds.soundcloud.com/stream/381349562-michael-olaya-297021510-level-1.mp3'
+    },
+    {
+        'title' : 'Level 2',
+        'url' : 'https://feeds.soundcloud.com/stream/381366890-michael-olaya-297021510-level2'
+    },
+    {
+        'title' : 'Level 3',
+        'url' : 'https://feeds.soundcloud.com/stream/381375959-michael-olaya-297021510-level3.mp3'
+    }
+];
+
+module.exports = audioData;
